7ecaa89319f725b7b6a0e0a846fea07eda3d00f0
Rohan Taneja <rohantaneja63@gmail.com>
2016-04-10 14:20:27 +0530

    initial commit

---
+++ omni_mediatek.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/mediatek/generic/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := mediatek
+PRODUCT_NAME := omni_mediatek
+PRODUCT_BRAND := mediatek
+PRODUCT_MANUFACTURER := mediatek
+PRODUCT_MODEL := Mediatek MT6582
