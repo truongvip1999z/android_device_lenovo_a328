a33807dc606bc97234a2afe1c28fd896325c30f2
truongvip1999z <truongvo18@gmail.com>
2016-11-06 13:40:25 +0700

    Update and rename Micromax_A311.mk to Lenovo_A328.mk

---
+++ Lenovo_A328.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/Lenovo/A328/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := A328
+PRODUCT_NAME := Lenovo_A328
+PRODUCT_BRAND := Lenovo
+PRODUCT_MANUFACTURER := Lenovo
+PRODUCT_MODEL := A328
--- Micromax_A311.mk
+++
@@ -1,9 +0,0 @@
-# Inherit device configuration
-$(call inherit-product, device/Micromax/A311/device.mk)
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := A311
-PRODUCT_NAME := Micromax_A311
-PRODUCT_BRAND := Micromax
-PRODUCT_MANUFACTURER := Micromax
-PRODUCT_MODEL := A311
